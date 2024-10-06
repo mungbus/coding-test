28c9e9730a50016c50662d0725c009f2b88bfc33
mungbus <gongdongho12@naver.com>
2024-10-06 21:49:12 +0900

    feat: 파티 고민 시작

---
+++ src/17week/Party.kt
@@ -0,0 +1,8 @@
+package `17week`
+
+fun main() {
+    val (N, M, X) = readln().split(" ").map { it.toInt() }
+    repeat(M - 1) {
+        val (A, B, T) = readln().split(" ").map { it.toInt() }
+    }
+}
