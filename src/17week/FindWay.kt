3542cc14f7c5ddb9d0d4ee97a8ab49153a8f71a0
mungbus <gongdongho12@naver.com>
2024-10-11 02:40:36 +0900

    chore: 경로 찾기 50퍼까지는 맞음

--- src/17week/FindWay.kt
+++ src/17week/FindWay.kt
@@ -5,33 +5,37 @@ fun main() {
     val adjacencyMatrix = Array(N) {
         Array(N) { 0 }
     }
-    val ijList: LinkedHashSet<Pair<Int, Int>> = LinkedHashSet<Pair<Int, Int>>()
+
+    fun LinkedHashSet<Pair<Int, Int>>.connections(i: Int, j: Int) {
+        val result = flatMap { (befI, befJ) ->
+            val tempList = LinkedHashSet<Pair<Int, Int>>()
+            if (befJ == i && adjacencyMatrix[befI][j] == 0) {
+                adjacencyMatrix[befI][j] = 1
+                tempList.add(befI to j)
+            }
+            if (befI == j && adjacencyMatrix[i][befJ] == 0) {
+                adjacencyMatrix[i][befJ] = 1
+                tempList.add(i to befJ)
+            }
+            tempList
+        }
+        if (result.isNotEmpty()) {
+            addAll(result)
+            result.forEach { (newI, newJ) ->
+                if (newI != i && newJ != j) {
+                    connections(newI, newJ)
+                }
+            }
+        }
+    }
+
+    val ijList: LinkedHashSet<Pair<Int, Int>> = LinkedHashSet()
     repeat(N) { i ->
         readln().split(" ").forEachIndexed { j, it ->
             if (it.toInt() >= 1) {
-                fun LinkedHashSet<Pair<Int, Int>>.connected(): Set<Pair<Int, Int>> {
-                    val result = flatMap { (befI, befJ) ->
-                        val tempList = LinkedHashSet<Pair<Int, Int>>()
-                        if (befJ == i) {
-                            adjacencyMatrix[befI][j] = 1
-                            tempList.add(befI to j)
-                        }
-                        if (befI == j) {
-                            adjacencyMatrix[i][befJ] = 1
-                            tempList.add(i to befJ)
-                        }
-                        tempList
-                    }.toSet()
-                    val check = this.addAll(result)
-                    return if (check) {
-                        this.connected()
-                    } else {
-                        this
-                    }
-                }
                 ijList.add(i to j)
                 adjacencyMatrix[i][j] = 1
-                ijList.connected()
+                ijList.connections(i, j)
             }
         }
     }
