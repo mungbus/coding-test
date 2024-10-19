6243a41973e4e0d1a11db45b41cea96460c98849
Dongho Kang <gongdongho12@naver.com>
2024-10-19 20:24:29 +0900

    chore: 수정사항 반영

--- src/18week/BulbAndSwitch.kt
+++ src/18week/BulbAndSwitch.kt
@@ -1,34 +1,38 @@
 package `18week`
 
-fun toggle(arr: IntArray, idx: Int) {
+fun IntArray.toggle(idx: Int) {
     for (i in idx - 1..idx + 1) {
-        if (i >= 0 && i < arr.size) {
-            arr[i] = 1 - arr[i] // 0이면 1로, 1이면 0으로
+        if (i in indices) {
+            this[i] = 1 - this[i]
         }
     }
 }
 
-fun minClicksToTarget(bulbs: IntArray, target: IntArray): Int {
-    val n = bulbs.size
+fun IntArray.applyToggle(after: IntArray, firstClick: Boolean): Int? {
+    val n = size
+    val temp = copyOf()
     var clicks = 0
-
-    for (i in 0 until n) {
-        if (bulbs[i] != target[i]) {
-            if (i + 1 < n) {
-                toggle(bulbs, i + 1)  // i번째가 다르면 그 다음 전구 클릭
-                clicks++
-            } else {
-                return -1  // 마지막 전구인데 다른 경우 불가능
-            }
+    if (firstClick) {
+        temp.toggle(0)
+        clicks++
+    }
+    for (i in 1..<n) {
+        if (temp[i - 1] != after[i - 1]) {
+            temp.toggle(i)
+            clicks++
         }
     }
 
-    return if (bulbs.contentEquals(target)) clicks else -1
+    return if (temp.contentEquals(after)) clicks else null
+}
+
+fun IntArray.minClicksToTarget(after: IntArray): Int {
+    return setOfNotNull(applyToggle(after, false), applyToggle(after, true)).minOrNull() ?: -1
 }
 
 fun main() {
     readln()
-    val before = readln().toCharArray().map { it.code }.toIntArray()
-    val after = readln().toCharArray().map { it.code }.toIntArray()
-    println(minClicksToTarget(before, after))
+    val before = readln().toCharArray().map { it.digitToInt() }.toIntArray()
+    val after = readln().toCharArray().map { it.digitToInt() }.toIntArray()
+    println(before.minClicksToTarget(after))
 }
