3b02fe8ea2150447b57480053388f148c7a82de1
Dongho Kang <gongdongho12@naver.com>
2024-10-19 16:06:59 +0900

    chore: 0만들기

---
+++ src/18week/MakeZero.kt
@@ -0,0 +1,14 @@
+package `18week`
+
+val operation = listOf("+", "-", "")
+
+fun main() {
+    val N = readln().toInt()
+    repeat(N) {
+        val list = (1..readln().toInt()).toList()
+
+        // 모든 값을 +, -, "" 이렇게 넣어서 js eval로 값을 연산한 뒤 그 값이 0인 대상만 출력하는 방식
+        println()
+    }
+
+}
