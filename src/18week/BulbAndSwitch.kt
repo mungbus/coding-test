cc32ed8002f94710ec441b58e2b3886ca28c992f
Dongho Kang <gongdongho12@naver.com>
2024-10-19 18:03:37 +0900

    chore: 전구 ㅂㄷ

--- src/18week/BulbAndSwitch.kt
+++ src/18week/BulbAndSwitch.kt
@@ -1,11 +1,34 @@
 package `18week`
 
-fun main() {
-    val N = readln().toInt()
-    val before = readln().split("").map { it.toInt() }
-    val after = readln().split("").map { it.toInt() }
+fun toggle(arr: IntArray, idx: Int) {
+    for (i in idx - 1..idx + 1) {
+        if (i >= 0 && i < arr.size) {
+            arr[i] = 1 - arr[i] // 0이면 1로, 1이면 0으로
+        }
+    }
+}
 
-    // 누를 곳 눌러서 상태값을 저장해야 하는데 이 상태값을 최적으로 하는 경우로 계산
-    // 1번 예시 110 -> 101 -> 010 방식 순
-    // 전후 값을 바꿔주는 버튼을 클릭하고 같이 뒤집어주면 끝
+fun minClicksToTarget(bulbs: IntArray, target: IntArray): Int {
+    val n = bulbs.size
+    var clicks = 0
+
+    for (i in 0 until n) {
+        if (bulbs[i] != target[i]) {
+            if (i + 1 < n) {
+                toggle(bulbs, i + 1)  // i번째가 다르면 그 다음 전구 클릭
+                clicks++
+            } else {
+                return -1  // 마지막 전구인데 다른 경우 불가능
+            }
+        }
+    }
+
+    return if (bulbs.contentEquals(target)) clicks else -1
+}
+
+fun main() {
+    readln()
+    val before = readln().toCharArray().map { it.code }.toIntArray()
+    val after = readln().toCharArray().map { it.code }.toIntArray()
+    println(minClicksToTarget(before, after))
 }
