ecf41c68634465b2c0795705bf4b9b0d3147d73b
Dongho Kang <gongdongho12@naver.com>
2024-10-19 15:43:49 +0900

    feat: 댕댕이 풀이 추가

--- src/18week/AvoidBombShotBluePuppy.kt
+++ src/18week/AvoidBombShotBluePuppy.kt
@@ -4,4 +4,6 @@ fun main() {
     val (N, M, T) = readln().split(" ").map { it.toInt() }
     val group = readln().split(" ").map { it.toInt() }
     val P = readln().toInt()
+
+    // 시간에 따라 이동할 수 있는 범위의 최댓값이 M이 되는지 체크하는 로직
 }
