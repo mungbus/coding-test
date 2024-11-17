32d54e384e83c659dde9c915ab25a83ae109c738
Dongho Kang <gongdongho12@naver.com>
2024-11-17 18:19:19 +0900

    feat: 창고다각형 풀이 완료

---
+++ src/19week/WarehousePolygon.kt
@@ -0,0 +1,48 @@
+package `19week`
+
+fun main() {
+    val N = readln().toInt()
+    var maxH = -1
+    val pipes = Array(N) {
+        val (L, H) = readln().split(" ").map { it.toInt() }
+        if (maxH <= H) {
+            maxH = H
+        }
+        Pair(L, H)
+    }
+    val pipeMap = pipes.associate { it.first to it.second }
+    var currentH = 0
+    var answer = 0
+    var afterMax = false
+    var afterMaxIndex = -1
+    val afterMaxList = mutableListOf<Pair<Int, Int>>()
+    for (i in pipeMap.keys.min()..pipeMap.keys.max()) {
+        val getH = pipeMap.get(i)
+        currentH = getH?.let {
+            if (it > currentH) it else currentH
+        } ?: currentH
+        if (afterMax && getH != null) {
+            afterMaxList.add(Pair(i, getH))
+        }
+        if (getH == maxH && afterMaxIndex < i) {
+            afterMax = true
+            afterMaxIndex = i
+        }
+        answer += currentH
+    }
+
+    val removePart = afterMaxList.filter { it.first > afterMaxIndex }
+    val removeMap = removePart.associate { it.first to it.second }
+    var remove = 0
+    currentH = 0
+    if (removePart.isNotEmpty()) {
+        for (i in removePart.last().first downTo afterMaxIndex + 1) {
+            val getH = removeMap.get(i)
+            currentH = getH?.let {
+                if (it > currentH) it else currentH
+            } ?: currentH
+            remove += maxH - currentH
+        }
+    }
+    println(answer - remove)
+}
