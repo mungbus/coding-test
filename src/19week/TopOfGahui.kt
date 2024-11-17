88212193aa1700a1a1f810b24087e8c0a1781458
Dongho Kang <gongdongho12@naver.com>
2024-11-17 16:35:53 +0900

    feat: 가희와 탑

---
+++ src/19week/TopOfGahui.kt
@@ -0,0 +1,28 @@
+package `19week`
+
+fun main() {
+    val (N, a, b) = readln().split(" ").map { it.toInt() }
+    val MIN = 1
+
+    val top = mutableListOf<Int>()
+
+    if (a + b > N + MIN) {
+        print(-1)
+        return
+    }
+
+    for (i in MIN..<a) {
+        top.add(i)
+    }
+
+    top.add(maxOf(a, b))
+
+    for (i in b - MIN downTo MIN) {
+        top.add(i)
+    }
+
+    while (top.size < N) {
+        top.add(if (a == 1) 1 else 0, MIN)
+    }
+    println(top.joinToString(" "))
+}
