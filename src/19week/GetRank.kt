0a32216e1e5a5c705bd60ac80dd79573fb4e8fe4
Dongho Kang <gongdongho12@naver.com>
2024-11-17 17:28:56 +0900

    feat: 등수구하기

---
+++ src/19week/GetRank.kt
@@ -0,0 +1,22 @@
+package `19week`
+
+import kotlin.math.min
+
+fun main() {
+    val (N, score, P) = readln().split(" ").map { it.toInt() }
+    val scores = if (N == 0) emptyList() else readln().split(" ").map { it.toInt() }
+    var topMin = -1
+    val currentTop = scores.filter {
+        val check = it >= score
+        if (check) {
+            topMin = if (topMin == -1) it else min(topMin, it)
+        }
+        check
+    }
+    if (currentTop.size < P) {
+        val beforeRank = currentTop.filter { if (topMin == score) it != topMin else true }.size
+        println(beforeRank + 1)
+    } else {
+        println(-1)
+    }
+}
