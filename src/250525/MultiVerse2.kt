0dac54bf812dca445acfbe833ae667b5bff2fe36
teddy.bear <teddy.bear@lgvoltup.com>
2025-05-24 16:57:38 +0900

    feat: 랭크로 전환해서 풀기

--- src/250525/MultiVerse2.kt
+++ src/250525/MultiVerse2.kt
@@ -1,29 +1,26 @@
 package `250525`
 
-fun spaceCalc(spaces: List<Int>): Int {
-	val temp = mutableListOf<Int>()
-	for (i in 0 until spaces.size) {
-		for (j in i until spaces.size) {
-			val append = if (spaces[i] > spaces[j]) 2
-			else if (spaces[i] < spaces[j]) 1
-			else 0
-			temp.add(append)
-		}
-	}
-	return temp.joinToString("").toInt(3)
+fun getRanks(space: List<Int>): List<Int> {
+    val sorted = space.withIndex().sortedBy { it.value }
+    val ranks = IntArray(space.size)
+    var rank = 0
+    for (i in sorted.indices) {
+        if (i > 0 && sorted[i].value != sorted[i - 1].value) rank++
+        ranks[sorted[i].index] = rank
+    }
+    return ranks.toList()
 }
 
-fun pairCount(n: Int): Int {
-	return n * (n - 1) / 2
-}
+fun pairCount(n: Int): Int = n * (n - 1) / 2
 
 fun main() {
-	val (M) = readln().split(" ").map { it.toInt() }
-	val spaces = List(M) {
-		val space = readln().split(" ").map { it.toInt() }
-		spaceCalc(space)
-	}
-	val spaceCount = spaces.groupingBy { it }.eachCount()
-	val result = spaceCount.values.filter { it > 1 }.sumOf { pairCount(it) }
-	println(result)
+    val (M) = readln().split(" ").map { it.toInt() }
+    val spaceCount = mutableMapOf<List<Int>, Int>()
+    repeat(M) {
+        val space = readln().split(" ").map { it.toInt() }
+        val ranks = getRanks(space)
+        spaceCount[ranks] = spaceCount.getOrDefault(ranks, 0) + 1
+    }
+    val result = spaceCount.values.filter { it > 1 }.sumOf { pairCount(it) }
+    println(result)
 }
