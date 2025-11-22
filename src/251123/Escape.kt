54d5887618d1c9633ae674a36d8f26d520db4c61
mungbus <gongdongho12@naver.com>
2025-11-22 22:13:26 +0900

    feat: 탈출 풀이 완료

--- src/251123/Escape.kt
+++ src/251123/Escape.kt
@@ -14,18 +14,22 @@ fun main() {
     val waterQueue = ArrayDeque<Point>()
     val hedgehogQueue = ArrayDeque<Point>()
 
-    // 물 도달 시간 기록 배열 (도달 불가능 시 MAX_VALUE)
-    val waterTime = Array(R) { IntArray(C) { Int.MAX_VALUE } }
+    val waterTime = Array(R) {
+        IntArray(C) {
+            Int.MAX_VALUE
+        }
+    }
 
-    var hedgehog: Point? = null
-    var startWater: Point? = null
+    var startHedgehog: Point? = null
 
-    // 초기화 및 시작점 찾기
-    for (r in 0 until R) {
-        for (c in 0 until C) {
+    repeat(R) { r ->
+        repeat(C) { c ->
             when (board[r][c]) {
-                'S' -> hedgehog = Point(r to c, 0)
-                '*' -> startWater = Point(r to c, 0)
+                'S' -> startHedgehog = Point(r to c, 0)
+                '*' -> {
+                    waterQueue.add(Point(r to c, 0))
+                    waterTime[r][c] = 0 // 물 시작점 시간 설정
+                }
             }
         }
     }
@@ -40,29 +44,23 @@ fun main() {
     fun isValid(pos: Pair<Int, Int>) = pos.first in 0 until R && pos.second in 0 until C
 
     // 각 칸에 물이 닿는 최소 시간 기록
-    if (startWater != null) {
-        waterQueue.add(startWater)
-        waterTime[startWater.pos.first][startWater.pos.second] = 0
+    while (waterQueue.isNotEmpty()) {
+        val (pos, time) = waterQueue.removeFirst()
 
-        while (waterQueue.isNotEmpty()) {
-            val (pos, time) = waterQueue.removeFirst()
+        directions.forEach { dir ->
+            val next = pos + dir
+            val (nr, nc) = next
 
-            directions.forEach { dir ->
-                val next = pos + dir
-                val (nr, nc) = next
-
-                if (isValid(next) && setOf('X', 'D').contains(board[nr][nc]) && waterTime[nr][nc] == Int.MAX_VALUE) {
-                    val nextTime = time + 1
-                    waterTime[nr][nc] = nextTime
-                    waterQueue.add(Point(next, nextTime))
-                }
-
+            if (isValid(next) && !setOf('X', 'D').contains(board[nr][nc]) && waterTime[nr][nc] == Int.MAX_VALUE) {
+                val nextTime = time + 1
+                waterTime[nr][nc] = nextTime
+                waterQueue.add(Point(next, nextTime))
             }
         }
     }
 
     // 고슴도치 이동, 물이 차기 전에 도착 가능한지 확인
-    hedgehog?.let { hedgehog ->
+    startHedgehog?.let { hedgehog ->
         hedgehogQueue.add(hedgehog)
         val visited = Array(R) { BooleanArray(C) }
         visited[hedgehog.pos.first][hedgehog.pos.second] = true
@@ -75,8 +73,8 @@ fun main() {
                 val (nr, nc) = next
 
                 if (isValid(next) && !visited[nr][nc]) {
-
                     val nextHedgehogTime = time + 1
+
                     // 비버굴 도착
                     if (board[nr][nc] == 'D') {
                         println(nextHedgehogTime)
